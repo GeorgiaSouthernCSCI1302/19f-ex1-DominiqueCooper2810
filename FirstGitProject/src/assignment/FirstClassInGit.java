a7a8cb2b2093df0e7cbd62a0c994f76c06ec2dd3
dc05809 <dc05809@CEIT2553214X030.ad.georgiasouthern.edu>
2019-08-21 16:51:04 -0400

    Collect user input, shows last name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,9 +1,15 @@
 package assignment;
+import java.util.Scanner;
 
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, my name is Dominique");
+		System.out.print("Type in your last name: ");
+		
+		Scanner input = new Scanner(System.in);
+		String name = input.nextLine();
+		
+		System.out.printf("Your name is %s", name);
 		
 		//add a statement to display your name in the console
 		
